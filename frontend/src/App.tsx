8377f3f1ec3ee8f387b65c0edf8a134446c278d4
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2026-01-18 15:58:56 +0200

    Improve UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,23 +6,24 @@ interface Meal {
 }
 
 function App() {
-  const [meal, setMeal] = useState<Meal>({ name: "" });
+  const [meal, setMeal] = useState<Meal>({ name: "Press the button..." });
 
   return (
     <div className="App">
-      <main>
-        <h1>I want to eat...</h1>
+      <div className="content">
+        <h1>I should make...</h1>
         <p>{meal.name}</p>
         <button
+          className="generate-button"
           onClick={async () => {
             const response = await fetch("http://localhost:8080/meals/random");
             const data = (await response.json()) as Meal;
             setMeal(data);
           }}
         >
-          Get a suggestion
+          Give a suggestion
         </button>
-      </main>
+      </div>
     </div>
   );
 }
