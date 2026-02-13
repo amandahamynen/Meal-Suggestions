81bde4b0d19ca1282cbf57bd1d9b27b795391e00
Amanda Hämynen <amandahamynen@users.noreply.github.com>
2026-02-13 14:16:19 +0200

    Add unit tests for adding and deleting meal

--- frontend/tests/services/meal.service.test.ts
+++ frontend/tests/services/meal.service.test.ts
@@ -1,7 +1,7 @@
 import mealService from "../../src/services/meal.service";
 import { describe, it, expect, vi, afterEach } from "vitest";
 import { Meal } from "../../src/types/meal";
-import { GET_MEALS_URL } from "../../src/constants/api.constants";
+import { MEALS_URL } from "../../src/constants/api.constants";
 
 const mockMeals: Meal[] = [
   { name: "Lihapullat" },
@@ -22,7 +22,7 @@ describe("meal.service", () => {
 
     const meals = await mealService.getMeals();
     expect(meals).toEqual(mockMeals);
-    expect(fetch).toHaveBeenCalledWith(GET_MEALS_URL);
+    expect(fetch).toHaveBeenCalledWith(MEALS_URL);
     expect(fetch).toHaveBeenCalledOnce();
   });
 
@@ -44,6 +44,65 @@ describe("meal.service", () => {
 
     try {
       await mealService.getMeals();
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect((error as Error).message).toBe(errorMessage);
+    }
+  });
+
+  it("should add a meal successfully", async () => {
+    vi.spyOn(globalThis, "fetch").mockResolvedValue({
+      ok: true,
+      json: async () => mockMeals[0],
+    } as Response);
+    const meal = await mealService.addMeal(mockMeals[0]);
+    expect(meal).toEqual(mockMeals[0]);
+    expect(fetch).toHaveBeenCalledWith(MEALS_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(mockMeals[0]),
+    });
+    expect(fetch).toHaveBeenCalledOnce();
+  });
+
+  it("should handle errors when adding a meal", async () => {
+    const errorMessage = "Failed to add meal";
+    vi.spyOn(globalThis, "fetch").mockResolvedValue({
+      ok: false,
+    } as Response);
+
+    try {
+      await mealService.addMeal(mockMeals[0]);
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect((error as Error).message).toBe(errorMessage);
+    }
+  });
+
+  it("should delete a meal successfully", async () => {
+    vi.spyOn(globalThis, "fetch").mockResolvedValue({
+      ok: true,
+    } as Response);
+    await mealService.deleteMeal(mockMeals[0]);
+    expect(fetch).toHaveBeenCalledWith(`${MEALS_URL}/${mockMeals[0]._id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    expect(fetch).toHaveBeenCalledOnce();
+  });
+
+  it("should handle errors when deleting a meal", async () => {
+    const errorMessage = "Failed to delete meal";
+    vi.spyOn(globalThis, "fetch").mockResolvedValue({
+      ok: false,
+    } as Response);
+
+    try {
+      await mealService.deleteMeal(mockMeals[0]);
     } catch (error) {
       expect(error).toBeInstanceOf(Error);
       expect((error as Error).message).toBe(errorMessage);
